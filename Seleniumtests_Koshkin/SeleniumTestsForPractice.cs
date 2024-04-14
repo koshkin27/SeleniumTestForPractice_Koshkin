f095a9e7b836fc6b861a55f0c3333935c2a07a2e
koshkin27 <dimakoshk@hotmail.com>
2024-04-14 17:08:32 +0500

    Мой первый коммит

---
+++ Seleniumtests_Koshkin/SeleniumTestsForPractice.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Seleniumtests_Koshkin;
+
+public class SeleniumTestsForPractice
+{
+    [Test]
+    public void Authorization()
+    {
+        var options = new ChromeOptions();
+        options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
+        
+        // зайти в хром (с помощью вебдрайвера)
+        var driver = new ChromeDriver(options);
+        
+        // перейти по урлу
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
+        Thread.Sleep(5000);
+        
+        // ввести логин и пароль
+        var login = driver.FindElement(By.Id("Username"));
+        login.SendKeys("dimakoshk@hotmail.com");
+
+        var password = driver.FindElement(By.Name("Password"));
+        password.SendKeys("Elephant27!");
+        
+        Thread.Sleep(3000);
+        
+        // нажать кнопку "войти"
+        var enter = driver.FindElement(By.Name("button"));
+        enter.Click();
+        Thread.Sleep(3000);
+        
+        //проверяем, что мы находимся на нужной странице
+        var currentUrl = driver.Url;
+        Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news");
+        
+        driver.Quit();
+    }
+}
